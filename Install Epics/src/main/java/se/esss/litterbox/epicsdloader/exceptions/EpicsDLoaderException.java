b0c47db381f16e0e298b9b9d100d5ee327188936
stevemolloy <stephen.molloy@ice-tech.systems>
2016-03-23 21:31:41 +0100

    First commit

---
+++ Install Epics/src/main/java/se/esss/litterbox/epicsdloader/exceptions/EpicsDLoaderException.java
@@ -0,0 +1,32 @@
+package se.esss.litterbox.epicsdloader.exceptions;
+
+public class EpicsDLoaderException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public EpicsDLoaderException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public EpicsDLoaderException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EpicsDLoaderException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EpicsDLoaderException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EpicsDLoaderException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
